f172c4970bf52a3225823f11cf30ce6ef709330f
animefn <admin@animefn.com>
2023-01-08 03:16:00 +0100

    test actions

--- github_release.ps1
+++ github_release.ps1
@@ -1,16 +1,11 @@
 # you have to run local_release first and try the version!
 
-param(
-    [string]$opath="local_release"
-    ) 
-
-#$original_path = pwd
-#cd -LiteralPath $opath
+param([string]$opath="local_release") 
 
 $Vc =  & "$opath\abst_cli.exe" -v
 $Vg= ((python .\gui\abst_gui.py  -v) -split ":")[1].replace(' ','')
 
-
-
 #"VER=${Vc}g${Vg}" >.github/ver.env
-echo  "VER=${Vc}g${Vg}" >.github/ver.env
+#echo  "VER=${Vc}g${Vg}" >.github/ver.env
+
+Set-Content -LiteralPath .github/ver.env -Value "VER=${Vc}g${Vg}"
