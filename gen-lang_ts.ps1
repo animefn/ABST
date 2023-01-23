cac5586a714e75221035f8beafbfaa47c85ed4a3
animefn <admin@animefn.com>
2023-01-23 15:55:20 +0100

    clean up .ts files

--- gen-lang_ts.ps1
+++ gen-lang_ts.ps1
@@ -2,4 +2,6 @@ mkdir -Force gui\lang_src
 #mkdir -Force gui\lang
 #pylupdate5  .\gui\abst_ui.py .\gui\abst_gui.py  -ts gui\lang_src\english.ts
 pylupdate5  .\gui\abst_ui.py .\gui\abst_gui.py  -ts gui\lang_src\arabic.ts
+# pylupdate5 -noobsolete  .\gui\abst_ui.py .\gui\abst_gui.py  -ts gui\lang_src\arabic.ts   
 pylupdate5  .\gui\abst_ui.py .\gui\abst_gui.py  -ts gui\lang_src\french.ts
+# pylupdate5   -noobsolete .\gui\abst_ui.py .\gui\abst_gui.py  -ts gui\lang_src\french.ts 
